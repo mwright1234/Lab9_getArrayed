4700f6f860e1f9f10f97ad2f8de8de3a0c720ba8
Marco Dempsey Wright <wrigh4md@mail.uc.edu>
2025-03-16 19:04:08 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Lab9_getArrayed {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random rnd = new Random();
+
+        // lab 1
+        int[] dataPoints = new int[100];
+
+
+
+
+
+
+
+
+    }
+}
